c5c99b8624f17cf67ea3dc89fb427e8642018e31
kevintnoooyen <Kevin4nguyen1@gmail.com>
2021-11-29 17:31:26 -0800

    Test

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -6,56 +6,24 @@ function Navigation(props) {
   return (
     <div className={styles.navigation}>
       <nav class={styles.navbar}>
-
-
           <div class={styles.navbar}>
-            <ul class="navbar-nav ml-auto">
-              <li
-                class={`nav-item  ${
-                  props.location.pathname === "/" ? "active" : ""
-                }`}
-              >
+            <ul class={styles.links}>
                 <Link class={styles.navlinkHome} to="/">
                   Home
                   <span class="sr-only">(current)</span>
                 </Link>
-              </li> 
-              <li
-                class={`nav-item  ${
-                  props.location.pathname === "/about" ? "active" : ""
-                }`}
-              >
                 <Link class={styles.navlinkAbout} to="/about">
                   About Me
                 </Link>
-              </li>
-              <li
-                class={`nav-item  ${
-                  props.location.pathname === "/projects" ? "active" : ""
-                }`}
-              >
                 <Link class={styles.navlinkProjects} to="/projects">
                   Projects
                 </Link>
-                </li>
-              <li
-                class={`nav-item  ${
-                  props.location.pathname === "/resume" ? "active" : ""
-                }`}
-              >
                 <Link class={styles.navlinkResume} to="/resume">
                   Resume
                 </Link>
-                </li>
-              <li
-                class={`nav-item  ${
-                  props.location.pathname === "/snapshots" ? "active" : ""
-                }`}
-              >
                 <Link class={styles.navlinkSnapshots} to="/snapshots">
                   Snapshots
                 </Link>
-              </li>
             </ul>
           </div>
       </nav>
